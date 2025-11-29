aaf599172133a7b4ceed59956ddb906859e304cf
codechelin <ccomercio100@gmail.com>
2025-11-29 16:12:21 +0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,12 @@ import { NextResponse, type NextRequest } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 
 export async function middleware(request: NextRequest) {
-  // Inicializa el cliente de Supabase con URL y Anon Key desde variables de entorno
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 
-  // Refresca la sesión si está expirada - requerido para Server Components
-  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
+  // Refresca la sesión si está expirada
   await supabase.auth.getSession()
 
   return NextResponse.next()
@@ -17,13 +15,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for las que empiezan con:
-     * - _next/static (archivos estáticos)
-     * - _next/image (archivos de optimización de imágenes)
-     * - favicon.ico (archivo favicon)
-     * Puedes modificar este patrón para incluir más rutas si lo necesitas.
-     */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
