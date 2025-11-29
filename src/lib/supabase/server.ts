03284716f3fd6ddea27283da691b8f9c7d2ad973
codechelin <ccomercio100@gmail.com>
2025-11-29 15:56:20 +0800

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -4,14 +4,14 @@ import { cookies } from 'next/headers'
 // Fuerza este módulo a ejecutarse en Node.js runtime
 export const runtime = "nodejs";
 
-export function createClient() {
-  const cookieStore = cookies()
+export async function createClient() {
+  const cookieStore = await cookies();
 
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
   
   if (!supabaseUrl || !supabaseKey) {
-    throw new Error("Supabase URL or Anon Key is missing from .env file.")
+    throw new Error("Supabase URL or Anon Key is missing from .env file.");
   }
 
   return createServerClient(
@@ -20,11 +20,11 @@ export function createClient() {
     {
       cookies: {
         get(name: string) {
-          return cookieStore.get(name)?.value
+          return cookieStore.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
           try {
-            cookieStore.set({ name, value, ...options })
+            cookieStore.set({ name, value, ...options });
           } catch (error) {
             // El método `set` fue llamado desde un Server Component.
             // Esto puede ignorarse si tienes middleware refrescando sesiones.
@@ -32,7 +32,7 @@ export function createClient() {
         },
         remove(name: string, options: CookieOptions) {
           try {
-            cookieStore.set({ name, value: '', ...options })
+            cookieStore.set({ name, value: '', ...options });
           } catch (error) {
             // El método `delete` fue llamado desde un Server Component.
             // Esto puede ignorarse si tienes middleware refrescando sesiones.
@@ -40,5 +40,5 @@ export function createClient() {
         },
       },
     }
-  )
+  );
 }
